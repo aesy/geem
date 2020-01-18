60ba736fa333960fe590a4366cb674862dd0a598
Isak W <isak.wertwein@gmail.com>
2020-01-18 23:09:01 +0100

    Fix game loop

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import 'normalize.css';
 const entities = [];
 const systems = [];
 
-function update(dt) {
+let lastTimestamp = 0;
+
+function update(currentTimestamp) {
+    const dt = (currentTimestamp - lastTimestamp) / 1000;
+    lastTimestamp = currentTimestamp;
+
     for (const system of systems) {
         const filteredEntities = entities.filter(system.appliesTo);
         system.update(dt, filteredEntities);
     }
+
+    window.requestAnimationFrame(update);
 }
+
+window.requestAnimationFrame(update);

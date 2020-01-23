f9a3bcfec4f2775893138f99fb14c0a9747b1576
mumis <emil.wertwein@gmail.com>
2020-01-23 11:16:09 +0100

    better worldgen, add simplex lib

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ import PointLight from './scripts/Entities/PointLight';
 import RotateSystem from './scripts/Systems/RotateSystem';
 import Grid from './scripts/Entities/Grid';
 import { generateWorld } from './scripts/WorldGen/randomWorld';
+import AmbientLight from './scripts/Entities/AmbientLight';
 
 const game = new Game();
 game.addSystem(new RenderSystem());
 game.addSystem(new FloatSystem());
-game.addSystem(new FrameRatePrinter());
-game.addSystem(new RotateSystem());
-game.addEntity(new Grid());
-game.addEntity(new PointLight());
+// game.addSystem(new FrameRatePrinter());
+// game.addSystem(new RotateSystem());
+// game.addEntity(new Grid());
+// game.addEntity(new PointLight());
+game.addEntity(new AmbientLight());
 
-for (const waterTile of generateWorld(10, 10)) {
-    game.addEntity(waterTile);
+for (const tile of generateWorld(60, 60, .2, 30, 14)) {
+    game.addEntity(tile);
 }
 
 game.start();
--- src/scripts/WorldGen/randomWorld.js
+++ src/scripts/WorldGen/randomWorld.js
@@ -1,16 +1,25 @@
 import GrassTile from '../Entities/GrassTile';
 import WaterTile from '../Entities/WaterTile';
+import { makeNoise2D } from 'open-simplex-noise';
 
-export function generateWorld(width, height) {
+export function generateWorld(width, height, frequency, amplitude, seed ) {
     const result = [];
+    let xOff = 0;
+    let zOff = 0;
+    const noise2D = makeNoise2D(seed);
+    for (let z = 0; z < height; z++) {
+        zOff = 0;
+        for (let x = 0; x < width; x++) {
+            let r = noise2D(zOff * frequency, xOff * frequency) * amplitude;
+            result.push(new GrassTile(x, Math.round(r), z));
+            zOff += 0.1;
+        }
+        xOff += 0.1;
+    }
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            if (Math.random() >= .5) {
-                result.push(new WaterTile(x, 0, y));
-            } else {
-                result.push(new GrassTile(x, 0, y));
-            }
+    for (let z = 0; z < height; z++) {
+        for (let x = 0; x < width; x++) {
+            result.push(new WaterTile(x, 0, z));
         }
     }
 
